54dcaadb95e4d21abcbcf165f8046c9764d70469
Orionas <100146226+Orionasis@users.noreply.github.com>
2022-10-21 12:28:35 +0200

    Add files via upload

--- function.js
+++ function.js
@@ -56,6 +56,25 @@ function delete_data(val){
             alert("fichier introuvable");
         }
     }
-    xhr.open("GET","update2.php?id="+val,true);
+    //xhr.open("GET","update2.php?id="+val,true);
+    xhr.open("GET","update_ajax.php?id="+val,true);
+    xhr.send(null);
+}
+function update_ajax(){
+    var nom=document.getElementById("nom_update").value;
+    var email=document.getElementById("email_update").value;
+    var id=document.getElementById("id_update").value;
+    var contenu=document.getElementById("contenu_update").value;
+    xhr=null;
+    xhr=new XMLHttpRequest();
+    xhr.onreadystatechange=function(){
+        if((4==xhr.readyState) && 200==xhr.status){
+            document.getElementById("messages").innerHTML=xhr.responseText;
+        }
+        if((4==xhr.readyState) && 404==xhr.status){
+            alert("fichier introuvable");
+        }
+    }
+    xhr.open("GET","update4.php?id="+id+"& nom="+nom+"&email="+email+"&contenu="+contenu,true);
     xhr.send(null);
 }
